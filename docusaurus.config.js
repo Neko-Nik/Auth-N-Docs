d6f7ba76703f20e739fe44ab16920e0a19474f74
Neko Nik <tandlenikhilraj@gmail.com>
2024-04-04 00:05:39 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -150,7 +150,7 @@ const config = {
         indexName: 'auth-n-nekonik',
 
         // Optional: see doc section below
-        contextualSearch: true,
+        contextualSearch: false,
 
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         // externalUrlRegex: 'nekonik\\.com|auth-n\\.nekonik\\.com|github\\.com|calendly\\.com|algolia\\.com',
